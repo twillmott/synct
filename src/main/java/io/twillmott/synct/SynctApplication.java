0476bdc3a25ae139c63de02ed87f327066405a5e
Tom Willmott <tom.willmott@hotmail.com>
2020-04-21 11:40:21 +0100

    Initial project structure

---
+++ src/main/java/io/twillmott/synct/SynctApplication.java
@@ -0,0 +1,13 @@
+package io.twillmott.synct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SynctApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SynctApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/twillmott/synct/SynctApplicationTests.java
@@ -0,0 +1,13 @@
+package io.twillmott.synct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SynctApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
